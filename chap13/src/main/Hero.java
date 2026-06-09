5771a9e94fbd4ba27ae9470ec66df87c3b1c38bd
SeiichiN <billie175@gmail.com>
2026-06-09 16:25:10 +0900

    6/9 p486 ゲッターセッター

---
+++ chap13/src/main/Hero.java
@@ -0,0 +1,53 @@
+package main;
+
+public class Hero {
+	private int hp;
+	private String name;
+	private Sword sword;
+	
+	// public Hero(String name) {
+	// 	this.name = name;
+	// }
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException
+			  ("名前がnullである。処理を中断");
+		}
+		if (name.length() <= 1) {
+			throw new IllegalArgumentException
+			  ("名前が短すぎる。処理を中断");
+		}
+		if (name.length() >= 8) {
+			throw new IllegalArgumentException
+			  ("名前が長過ぎる。処理を中断");
+		}
+		
+	 	this.name = name;
+	}
+	
+	public void attack(Matango m) {
+		System.out.println("おばけキノコからの反撃");
+		this.hp -= 2;
+		if (this.hp <= 0) {
+			this.die();
+		}
+	}
+	
+	private void die() {
+		System.out.println(this.name + "は死んでしまった");
+		System.out.println("GAME OVERです");
+	}
+	
+	void sleep() {
+		this.hp = 100;
+		System.out.println
+		  (this.name + "は眠って回復した");
+	}
+	
+
+}
---
+++ chap13/src/main/Sword.java
@@ -0,0 +1,6 @@
+package main;
+
+public class Sword {
+	String name;
+	int damage;
+}
